bbb4ecaebdd3f70440c6cabf8b796476e441df0d
Arnaud Lemercier <arnaud@wixiweb.fr>
2016-10-30 00:42:04 +0200

    feat: Ajout des actions fenêtre
     Loading au chargement
     Action pour "magnétiser" la fenêtre en bas à droite de l'écran
     Action pour "épingler" la fenêtre au dessus des autres
     Action pour fermer la fenêtre

--- src/views/renderer.js
+++ src/views/renderer.js
@@ -1,3 +1,72 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const electron = require('electron');
+const remote = electron.remote;
+const display = electron.screen;
+let BrowserWindow = remote.getCurrentWindow();
+
+const ipc = electron.ipcRenderer;
+let close = document.querySelector('.js-close');
+let pin = document.querySelector('.js-pin');
+let magnetic = document.querySelector('.js-magnetic');
+let toolbar = document.querySelector('.js-toolbar');
+let title = document.querySelector(".js-title");
+let wait = document.querySelector(".js-wait");
+
+let webview = document.getElementById("view");
+
+
+close.addEventListener('click', function () {
+    ipc.send('close-main-window');
+});
+
+pin.addEventListener('click', function () {
+    pin.classList.toggle('is-pinned');
+    if (pin.classList.contains('is-pinned')) {
+        BrowserWindow.setAlwaysOnTop(true);
+    } else {
+        BrowserWindow.setAlwaysOnTop(false);
+    }
+});
+
+magnetic.addEventListener('click', function () {
+    magnetic.classList.toggle('is-magnetized');
+    if (magnetic.classList.contains('is-magnetized')) {
+        BrowserWindow.setPosition(
+            display.getPrimaryDisplay().workAreaSize.width - BrowserWindow.getBounds().width,
+            display.getPrimaryDisplay().workAreaSize.height - BrowserWindow.getBounds().height
+        );
+        toolbar.classList.remove('drag');
+    } else {
+        toolbar.classList.add('drag');
+    }
+});
+
+BrowserWindow.on('resize', function () {
+    if (magnetic.classList.contains('is-magnetized')) {
+        magnetic.click();
+    }
+});
+
+ipc.on('view-load-url', function (e, msg) {
+    webview.src = msg;
+});
+
+
+webview.addEventListener("did-get-redirect-request", function (e) {
+    if (e.newURL.lastIndexOf('https://www.google.com/accounts/ServiceLogin') === 0) {
+        title.innerHTML = "Auth";
+    }
+});
+
+
+webview.addEventListener("did-start-loading", function () {
+    title.innerHTML = "loading...";
+});
+
+webview.addEventListener("did-stop-loading", function () {
+    title.innerHTML = "Google Task";
+    wait.style.display = 'none';
+});
+
+webview.addEventListener('did-finish-load', function () {
+    webview.insertCSS("html, body{background:white!important}");
+});
